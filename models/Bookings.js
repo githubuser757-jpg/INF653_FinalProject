0c79588f74ddaa54fe0a355b7c4f345940fe48d7
githubuser757-jpg <supremecommander757@gmail.com>
2025-05-08 17:39:05 -0400

    Update Bookings.js

--- models/Bookings.js
+++ models/Bookings.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const BookingsSchema = mongoose.Schema({
-=======
-const BookingsSchema = module.Schema({
->>>>>>> cac34aa822dcbc141001fc7b2505be23661824a1
     user: {
         type: mongoose.Schema.Types.ObjectID,
         ref: "Users",
